1a572aa3c539d2bb3e3db6ac9a82aeabdf533715
Joseal19 <albertoj0988@gmail.com>
2024-05-01 13:36:08 -0300

    refactor: ensure product list receives products as prop

--- app/_components/product-list.tsx
+++ app/_components/product-list.tsx
@@ -1,18 +1,21 @@
+import { Prisma } from "@prisma/client";
 import { db } from "../_lib/prisma";
 import ProductItem from "./product-item";
 
-const ProductList = async () => {
-  const products = await db.product.findMany({
-    where: {
-      discountPercentage: {
-        gt: 0,
-      },
-    },
-    take: 10,
-    include: {
-      restaurant: { select: { name: true } },
-    },
-  });
+
+interface ProductListProps {
+    products: Prisma.ProductGetPayload<{
+        include: {
+          restaurant: {
+            select: {
+              name: true;
+            };
+          };
+        };
+      }>[];
+}
+
+const ProductList = async ({products}: ProductListProps) => {
   return (
     // esconde o scroll [&::-webkit-scrollbar]:hidden
     <div className="flex overflow-x-scroll [&::-webkit-scrollbar]:hidden gap-4 px-5">
