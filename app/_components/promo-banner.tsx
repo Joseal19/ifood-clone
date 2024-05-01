5a106de1de54cb54b8d2b02349755dc1ee7f80d9
Joseal19 <albertoj0988@gmail.com>
2024-05-01 13:43:46 -0300

    refactor: promo banner

--- app/_components/promo-banner.tsx
+++ app/_components/promo-banner.tsx
@@ -1,15 +1,7 @@
 import Image, {ImageProps} from "next/image";
-
-
-interface PromoBannerProps {
-        
-}
-
 const PromoBanner = (props: ImageProps) => {
     return ( 
         <Image
-          src="/promo-banner-01.png"
-          alt="Até 30% de desconto em pizza."
           width={0}
           height={0}
           className="w-full h-auto object-contain"
