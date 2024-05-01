7b35565ac60fb6358c72771bac4ffe09a4baf18f
Joseal19 <albertoj0988@gmail.com>
2024-05-01 12:27:38 -0300

    style: indent code properly

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,30 @@
-import Search  from "./_components/search";
+import Search from "./_components/search";
 import Header from "./_components/header";
 import CategoryList from "./_components/category-list";
+import Image from "next/image";
 
 const Home = () => {
   return (
-   <>
-    <div className="px-5 pt-6">
-      <Header />
-      <Search />
+    <>
+      <div className="px-5 pt-6">
+        <Header />
+        <Search />
 
-      <CategoryList/>
-    </div>
-   </>
-  )
-}
-
+        <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizza."
+          width={0}
+          height={0}
+          className="w-full h-auto object-contain"
+          sizes="100vw"
+          quality={100}
+        />
+      </div>
+    </>
+  );
+};
 
 export default Home;
