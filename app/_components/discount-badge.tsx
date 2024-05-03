03f1b33937381411c9a96546cf3b35f99dd24d1f
Joseal19 <albertoj0988@gmail.com>
2024-05-02 21:12:33 -0300

    feat: add discount badge components

---
+++ app/_components/discount-badge.tsx
@@ -0,0 +1,19 @@
+import { Product } from "@prisma/client";
+import { ArrowDownIcon } from "lucide-react";
+
+interface DiscountBadgeProps {
+  product: Pick<Product, "discountPercentage">;
+}
+
+const DiscountBadge = ({ product }: DiscountBadgeProps) => {
+  return (
+    <div className="flex gap-[2px] bg-[#EA1D2C] py-[2px] px-2 items-center rounded-full text-white">
+      <ArrowDownIcon size={12} />
+      <span className="font-semibold text-xs">
+        {product.discountPercentage}%
+      </span>
+    </div>
+  );
+};
+
+export default DiscountBadge;
